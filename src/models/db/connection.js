3b4d8b179b754663e92477abc8ec1df5c73c9504
Fardeenmirza <64380155+Fardeenmirza@users.noreply.github.com>
2021-04-12 16:26:11 +0530

    Add files via upload

--- src/models/db/connection.js
+++ src/models/db/connection.js
@@ -1,1 +1,10 @@
-
+const mongoose = require('mongoose');
+mongoose.connect("mongodb://localhost:27017/salonTable-api",{
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    useCreateIndex: true
+}).then(()=>{
+    console.log("connection is successfull...");
+}).catch((err)=> {
+    console.log("no connection");
+})   
